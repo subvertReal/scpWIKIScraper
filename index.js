786b58c7ab2a891c20b33b66f92d2d784ed4cf33
subvertReal <sherjeelk2004@gmail.com>
2026-04-23 17:21:19 -0400

    pulling the item ids

--- index.js
+++ index.js
@@ -1,120 +1,126 @@
 const axios = require('axios');
 const path = require('path');
 const fs = require('fs');
- 
-// const url = 'https://scp-wiki.wikidot.com/scp-series';
-// axios.get(url)
-//   .then(response => {
-//     const html = response.data;
-//     fs.writeFile('download.html', html, (err) => {
-//       if (err) {
-//         console.error('Error saving file:', err);
-//       } else {
-//         console.log('HTML saved to page.html');
-//       }
-//     });
-//   })
-//   .catch(error => {
-//     console.error('Error fetching HTML:', error);
-//   });
+const cheerio = require('cheerio');
 
-// ! for all items 002 to 999 that are in html format
-function createPath (pathNew){
-    try {
-      if (!fs.existsSync(pathNew)) {
-        fs.mkdirSync(pathNew);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-}
+function getItemIds(url){
+  axios.get(url)
+  .then(response=> {
+    const html = response.data;
+    let $ = cheerio.load(html);
 
-// Creates all needed paths
-let pathsCreating = ['html','html/item'];
-Object.keys(pathsCreating).forEach(pathKey => {
-  createPath(pathsCreating[pathKey]);
-  console.log(pathsCreating[pathKey]);
+    let $li = $('li:contains("SCP-")');
+    let iID = $('$li');
+    console.log($li);
+    
+    let results = [];
+    $li.each((i, el) => {
+    let text = $(el).text().trim();
+    let id = text.split(' ')[0];
+    results.push(id);
 });
 
+    fs.writeFileSync('a.txt', results.join('\n'));
+    console.log(results);
 
+  })
+}
 
-let scpItemUrls = ['https://raw.githubusercontent.com/scp-data/scp-api/refs/heads/main/docs/data/scp/items/content_series-1.json'];
+function downloadItem(){
+// cheerio loads the html file that is read
 
-// function getSCPItems(itemURL){
-//   let url = scpItemUrls[0];
+let $ = cheerio.load(fs.readFileSync('editFile.html'), null, false);
 
-//   axios.get(url)
-//   .then(response => {
-//     let data = response.data;
+// get the page title for filename
+let itemNum = $('title').text();
+itemNum = itemNum.replace(/ .*/, "");
 
-//     Object.keys(data).forEach(key => {
-//     newJson = {
-      
-//     }
-//     let numSCP = key;
-//     newJson.itemNumber = numSCP;
-//     newJson.description = data[key].raw_content;
-    
-//     let jsonData = JSON.stringify(newJson, null, 2);
-    
-//     let paths
-//     paths = path.join('html/item',`${numSCP}.json`);
-//     // console.log(paths);
-//     fs.writeFile(paths, jsonData, 'utf8', (err) => {
-//         if (err) {
-//             console.error('Error writing to file', err);
-//         } else {
-//             console.log('Data written to file');
-//         }
-//     });
-      
-//   });
-//   })
-//   .catch(error => {
-//     console.error('Error fetching HTML:', error);
-//   });
+console.log(itemNum);
 
-// }
+// bars
+$('#side-bar').remove();
+$('#top-bar').remove();
 
-function getSCPItems(itemURL){
-  let url = scpItemUrls[0];
-
-  axios.get(url)
-  .then(response => {
-    let data = response.data;
-
-    Object.keys(data).forEach(key => {
-    newJson = {
-      
+//remove the width of the content
+$('#content-wrap').append(`
+  <style>
+    #content-wrap {
+      max-width: none !important;
+      width: 100% !important;
     }
-    let numSCP = key;
-    newJson.itemNumber = numSCP;
-    newJson.description = data[key].raw_content;
-    
-    let jsonData = JSON.stringify(newJson, null, 2);
-    
-    let paths
-    paths = path.join('html/item',`${numSCP}.json`);
-    // console.log(paths);
-    fs.writeFile(paths, jsonData, 'utf8', (err) => {
-        if (err) {
-            console.error('Error writing to file', err);
-        } else {
-            console.log('Data written to file');
-        }
+  </style>
+`);
+
+// wiki elements that wont work
+$('.rateup').remove();
+$('.ratedown').remove();
+$('#search-top-box').remove();
+$('#login-status').remove();
+$('.cancel.btn.btn-default').remove();
+
+// footer
+$('.footer-wikiwalk-nav').remove();
+$('#footer').remove();
+$('#page-options-bottom').remove();
+
+// SCP-2000 bibitem edit, hopefully doesnt break other pages. The bib entries lose line spacings for some reason, so this should add them back
+$('.bibitem').append(
+  `
+    <style>
+      .bibitem { 
+        padding-bottom: 2em;
+        margin-bottom: 3em;
+      }
+    </style>
+  `
+);
+
+
+let reg = $.html();
+
+
+
+let paths = path.join('html',`${itemNum}.html`);
+
+fs.writeFileSync(`${itemNum}.html`, reg);
+fs.writeFile(paths, reg, (err) => {
+      if (err) {
+        console.error('Error saving file:', err);
+      } else {
+        console.log(`HTML saved to ${itemNum}.html`);
+
+      }
     });
-      
-  });
+
+}
+
+
+
+
+// downloads file from wiki
+let seriesItemUrl = 'https://scp-wiki.wikidot.com/scp-series-10';
+
+let itemIds = [];
+getItemIds(seriesItemUrl);
+
+const url = 'https://scp-wiki.wikidot.com/scp-2000';
+axios.get(url)
+  .then(response => {
+    const html = response.data;
+
+    // let paths = path.join('test',`edit.html`);
+    let paths = path.join(`editFile.html`);
+
+    fs.writeFile(paths, html, (err) => {
+      if (err) {
+        console.error('Error saving file:', err);
+      } else {
+        console.log('HTML saved to editFile.html');
+        downloadItem();
+      }
+    });
   })
   .catch(error => {
     console.error('Error fetching HTML:', error);
   });
 
-}
-
-let i = -1;
-while(i<(Object.keys(scpItemUrls)+1)){
-  console.log(i);
-  getSCPItems(scpItemUrls[i+1])
-  i++
-}
